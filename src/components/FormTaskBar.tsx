1668b204cd8aa98a9d8f1656d6f3884883360017
odiogoaraujo <diogoaraujopereira18@gmail.com>
2023-10-17 03:18:38 -0300

    feat: clearing of input field after form submit

--- src/components/FormTaskBar.tsx
+++ src/components/FormTaskBar.tsx
@@ -1,22 +1,27 @@
-import { Input, Button, Form } from "./FormTaskBar.styled";
-import { TaskContext } from "../contexts/TaskContext";
-import { useContext } from "react";
+import { Input, Button, Form } from "./FormTaskBar.styled"
+import { TaskContext } from "../contexts/TaskContext"
+import { useContext, useState } from "react"
 import AddIcon from "./add-icon"
 
 export function FormTaskBar() {
-    const { addTask } = useContext(TaskContext);
+    const { addTask } = useContext(TaskContext)
+    const [inputValue, setInputValue] = useState('')
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-
-        const taskInputField = event.currentTarget.elements.namedItem('taskInput')
-        const text = (taskInputField as HTMLInputElement).value
-        addTask(text)
+        addTask(inputValue)
+        setInputValue('')
     }
 
     return (
         <Form onSubmit={handleSubmit}>
-            <Input type="text" placeholder="Adicione uma nova tarefa" name="taskInput" />
+            <Input
+                type="text"
+                placeholder="Adicione uma nova tarefa"
+                name="taskInput"
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+            />
             <Button>
                 Criar
                 <AddIcon />
